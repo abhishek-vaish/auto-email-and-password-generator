b98b3ebad0481b982f5aec648a51470b4bd7123a
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2020-04-11 17:40:43 +0530

    Helper class updated

--- src/helper/Helper.java
+++ src/helper/Helper.java
@@ -8,5 +8,54 @@
 
 package helper;
 
+import user.UserDetails;
+
 public class Helper {
+
+    UserDetails userDetails = new UserDetails();
+
+    public String createUserEmail() {
+        return String.format("%s_%d@%s.com", userDetails.getUserName().toLowerCase(), userDetails.getYearOfJoining(), userDetails.getCompanyName().toLowerCase());
+    }
+
+    private int generateRandomIntegerFromLimit(int limit) {
+        return (int) (Math.random() * limit);
+    }
+
+    private char generateRandomCharacter() {
+        return (char) ('a' + generateRandomIntegerFromLimit(26));
+    }
+
+    private String getFirstFiveCharacterFromUserName() {
+        if (userDetails.getUserName().length() < 5) {
+            StringBuilder pass = new StringBuilder();
+            int difference = 5 - userDetails.getUserName().length();
+            pass.append("0".repeat(Math.max(0, difference)));
+            return pass.toString();
+        } else {
+            return userDetails.getUserName().substring(0, 5);
+        }
+    }
+
+    private char getGenerateRandomSpecialSymbol() {
+        char[] specialSymbol = {
+                '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '{', '}', '[', ']', '\\', '\'', '\"',
+                '<', '>', '?', '/', ',', '.'
+        };
+        int randomNumber = generateRandomIntegerFromLimit(specialSymbol.length);
+        return specialSymbol[randomNumber];
+    }
+
+    public String createUserPassword() {
+        StringBuilder pass = new StringBuilder();
+        pass.append(generateRandomCharacter());
+        int j = 0;
+        while (j < 4) {
+            pass.append((int) (Math.random() * 10));
+            j++;
+        }
+        pass.append(getFirstFiveCharacterFromUserName());
+        pass.append(getGenerateRandomSpecialSymbol());
+        return pass.toString();
+    }
 }
